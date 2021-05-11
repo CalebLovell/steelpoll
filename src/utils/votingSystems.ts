417039b8db61feaa4caebbc9df1fb6bd936df646
Caleb Lovell <caleblovell1@gmail.com>
2021-05-10 20:25:30 -0600

    add vote system descriptions

--- src/utils/votingSystems.ts
+++ src/utils/votingSystems.ts
@@ -1,5 +1,23 @@
 export const votingSystems = [
-	{ id: 0, slug: `first-past-the-post`, type: `plurality`, name: `First Past the Post`, description: `an example description here` },
-	{ id: 1, slug: `ranked-choice`, type: `ordinal`, name: `Ranked Choice`, description: `an example description here` },
-	{ id: 2, slug: `STAR`, type: `cardinal`, name: `STAR`, description: `an example description here` },
+	{
+		id: 0,
+		slug: `first-past-the-post`,
+		type: `plurality`,
+		name: `First Past the Post`,
+		description: `Also known as Winner Take All, the choice with a plurality of votes wins.`,
+	},
+	{
+		id: 1,
+		slug: `ranked-choice`,
+		type: `ordinal`,
+		name: `Ranked Choice`,
+		description: `Rank each choice in order of preference. SteelPoll uses a method called the Borda count to calculate the winner. This system will select a more broadly popular choice in comparison to First Past the Post voting.`,
+	},
+	{
+		id: 2,
+		slug: `STAR`,
+		type: `cardinal`,
+		name: `Score then Automatic Runoff (STAR)`,
+		description: `Rank each choice between 1 and 5. The two choices with the highest total scores will compete in an instant runoff, which will select the choice preferred on more ballots as the final winner. This system allows voters to weight their preferences.`,
+	},
 ];
