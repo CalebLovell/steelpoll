333a6c631e372a1d38325a519819c2bc8aaf97ff
Caleb Lovell <caleblovell1@gmail.com>
2021-05-11 18:08:04 -0600

    add localePath for i18n

--- src/pages/poll/[pollId]/results.tsx
+++ src/pages/poll/[pollId]/results.tsx
@@ -1,42 +1,38 @@
 import * as React from 'react';
 
-import { useAuthUser, withAuthUser } from 'next-firebase-auth';
+import { useAuthUser, withAuthUser, withAuthUserTokenSSR } from 'next-firebase-auth';
 
 import { Container } from '@components/Container';
-
-// import { ResultsTable } from '@components/ResultsTable';
-// import dynamic from 'next/dynamic';
-// import { usePoll } from '@hooks/polls';
-// import { useResults } from '@hooks/votes';
-// import { useRouter } from 'next/router';
-
-// import { withAuthUserTokenSSR } from 'next-firebase-auth';
-
-// import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { ResultsTable } from '@components/ResultsTable';
+import dynamic from 'next/dynamic';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { usePoll } from '@hooks/polls';
+import { useResults } from '@hooks/votes';
+import { useRouter } from 'next/router';
 
 // import { useTranslation } from 'react-i18next';
 
-// const DynamicChart = dynamic(() => import(`@components/PieChart`).then(mod => mod.PieChart));
+const DynamicChart = dynamic(() => import(`@components/PieChart`).then(mod => mod.PieChart));
 
-// export const getServerSideProps = withAuthUserTokenSSR()(async ({ locale }) => {
-// 	// @ts-ignore
-// 	const translations = await serverSideTranslations(locale, [`common`]);
-// 	return {
-// 		props: {
-// 			...translations,
-// 		},
-// 	};
-// });
+export const getServerSideProps = withAuthUserTokenSSR()(async ({ locale }) => {
+	// @ts-ignore
+	const translations = await serverSideTranslations(locale, [`common`]);
+	return {
+		props: {
+			...translations,
+		},
+	};
+});
 
 const ResultsPage = () => {
 	const authUser = useAuthUser();
 	// const { t: home } = useTranslation(`home`);
-	// const router = useRouter();
-	// const { pollId } = router.query;
+	const router = useRouter();
+	const { pollId } = router.query;
 	// @ts-ignore
-	// const { data: votes, isLoading, fptpResults, rankedChoiceResults, STARResults } = useResults(pollId);
+	const { data: votes, isLoading, fptpResults, rankedChoiceResults, STARResults } = useResults(pollId);
 	// @ts-ignore
-	// const { data: poll } = usePoll(pollId);
+	const { data: poll } = usePoll(pollId);
 
 	const tabs = [
 		{ name: `First Past The Post`, href: `#`, current: false },
@@ -91,7 +87,7 @@ const ResultsPage = () => {
 							</div>
 						</div>
 					</div>
-					{/* {isLoading && <span>Collection: Loading...</span>}
+					{isLoading && <span>Collection: Loading...</span>}
 					{votes && poll && fptpResults && (
 						<div>
 							FPTP Winner(s):
@@ -116,7 +112,7 @@ const ResultsPage = () => {
 							<ResultsTable results={STARResults} poll={poll} isPercent={true} />
 							<DynamicChart data={STARResults?.votes} isPercent={true} />
 						</div>
-					)} */}
+					)}
 				</section>
 			</main>
 		</Container>
