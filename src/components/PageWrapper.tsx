3f6d07a9dbe3054897dee499c301447f36bf5600
Caleb Lovell <caleblovell1@gmail.com>
2021-05-26 22:54:27 -0600

    fix relative location link href

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -36,8 +36,13 @@ export const PageWrapper: React.FC<Props> = ({
 					<meta name='description' content={description} />
 					<meta charSet='UTF-8' />
 					<meta name='viewport' content='width=device-width, initial-scale=1' />
-					<meta name='msapplication-TileColor' content='#3b82f6' />
+					<meta name='msapplication-TileColor' content='#2b5797' />
 					<meta name='theme-color' content='#ffffff' />
+					<link rel='mask-icon' href='/favicons/safari-pinned-tab.svg' color='#5bbad5' />
+					<link rel='apple-touch-icon' sizes='180x180' href='/favicons/apple-touch-icon.png' />
+					<link rel='icon' type='image/png' sizes='32x32' href='/favicons/favicon-32x32.png' />
+					<link rel='icon' type='image/png' sizes='16x16' href='/favicons/favicon-16x16.png' />
+					<link rel='manifest' href='/favicons/site.webmanifest' />
 					<meta property='og:image' content={image} key='ogimage' />
 					<meta property='og:image:width' content='1200' key='ogimagewidth' />
 					<meta property='og:image:height' content='630' key='ogimageheight' />
