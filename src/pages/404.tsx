75ce5b8a4b26b34bc2d9351ea6d4f089e5dc7712
Caleb Lovell <caleblovell1@gmail.com>
2021-06-01 01:31:43 -0600

    add 404 i18n

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,19 +2,21 @@ import { useAuthUser, withAuthUser } from 'next-firebase-auth';
 
 import { PageWrapper } from '@components/PageWrapper';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'react-i18next';
 
 const Custom404 = () => {
 	const authUser = useAuthUser();
+	const { t: common } = useTranslation(`common`);
 	const metadata = {
-		title: `Error 404 - SteelPoll`,
-		description: `This page could not be found!`,
+		title: `${common(`errors.404.name`)} - SteelPoll`,
+		description: common(`errors.404.description`),
 	};
 	return (
 		<PageWrapper authUser={authUser} metadata={metadata}>
 			<main className='container flex items-center justify-center bg-brand-primary min-h-content'>
 				<div className='flex flex-col items-center justify-center w-full h-full sm:flex-row'>
-					<h1 className='pb-4 mb-4 text-4xl border-b-2 sm:mb-0 sm:pb-0 sm:pr-4 sm:border-b-0 sm:border-r-2 text-brand-primary'>Error 404</h1>
-					<h2 className='text-xl sm:ml-4 text-brand-primary'>This page could not be found!</h2>
+					<h1 className='pb-4 mb-4 text-4xl border-b-2 sm:mb-0 sm:pb-0 sm:pr-4 sm:border-b-0 sm:border-r-2 text-brand-primary'>{common(`errors.404.name`)}</h1>
+					<h2 className='text-xl sm:ml-4 text-brand-primary'>{common(`errors.404.description`)}</h2>
 				</div>
 			</main>
 		</PageWrapper>
@@ -22,7 +24,7 @@ const Custom404 = () => {
 };
 
 export const getStaticProps = async ({ locale }) => {
-	const translations = await serverSideTranslations(locale, [`common`, `home`]);
+	const translations = await serverSideTranslations(locale, [`common`]);
 	return {
 		props: {
 			...translations,
