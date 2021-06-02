417810351faf098e2c02ff5b27fd784508c5a598
Caleb Lovell <caleblovell1@gmail.com>
2021-06-02 03:18:08 -0600

    remove secure image

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -40,7 +40,6 @@ export const PageWrapper: React.FC<Props> = ({ metadata, authUser, children }) =
 					<meta property='og:site_name' content='SteelPoll' />
 					<meta property='og:type' content='website' />
 					<meta property='og:image' content={image} />
-					<meta property='og:image:secure_url' content={image} />
 					<meta property='og:image:type' content='image/png' />
 					<meta property='og:image:width' content='1200' />
 					<meta property='og:image:height' content='630' />
