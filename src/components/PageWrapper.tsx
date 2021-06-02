b1964f5d10255f814840f889f6019dd430ceee57
Caleb Lovell <caleblovell1@gmail.com>
2021-06-02 03:08:02 -0600

    more nonsense. linkedin pls

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -42,8 +42,8 @@ export const PageWrapper: React.FC<Props> = ({ metadata, authUser, children }) =
 					<meta property='og:image' content={image} />
 					<meta property='og:image:secure_url' content={image} />
 					<meta property='og:image:type' content='image/png' />
-					<meta property='og:image:width' content='600' />
-					<meta property='og:image:height' content='300' />
+					<meta property='og:image:width' content='1200' />
+					<meta property='og:image:height' content='630' />
 					<meta name='twitter:card' content='summary' />
 					<meta name='twitter:site' content='@Caleb__Lovell' />
 					<meta name='twitter:title' content={title} />
