d2cef3d8874a4195357b06221c852b1eebcf72c0
Caleb Lovell <caleblovell1@gmail.com>
2021-06-02 03:40:17 -0600

    no sizes

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -41,8 +41,6 @@ export const PageWrapper: React.FC<Props> = ({ metadata, authUser, children }) =
 					<meta property='og:type' content='website' />
 					<meta property='og:image' content={image} />
 					<meta property='og:image:type' content='image/png' />
-					<meta property='og:image:width' content='1200' />
-					<meta property='og:image:height' content='630' />
 					<meta name='twitter:card' content='summary' />
 					<meta name='twitter:site' content='@Caleb__Lovell' />
 					<meta name='twitter:title' content={title} />
