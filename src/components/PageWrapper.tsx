cce41d8a74c12f9a33c9c15bce68bb52bc9aac8b
Caleb Lovell <caleblovell1@gmail.com>
2021-06-02 17:20:50 -0600

    change twitter card to see if it changes discord

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -40,13 +40,13 @@ export const PageWrapper: React.FC<Props> = ({ metadata, authUser, children }) =
 					<meta property='og:site_name' content='SteelPoll' />
 					<meta property='og:type' content='website' />
 					<meta property='og:image' content={image} />
-					<meta property='og:image:width' content='600' />
-					<meta property='og:image:height' content='600' />
+					<meta property='og:image:width' content='1200' />
+					<meta property='og:image:height' content='627' />
 					<meta property='og:image:type' content='image/png' />
-					<meta name='twitter:card' content='summary' />
+					<meta name='twitter:card' content='summary_large_image' />
 					<meta name='twitter:site' content='@Caleb__Lovell' />
 					<meta name='twitter:title' content={title} />
-					<meta name='twitter:image' content='https://www.steelpoll.com/images/steelpoll_pie_chart.png' />
+					<meta name='twitter:image' content={image} />
 					<meta name='twitter:description' content={description} />
 					<meta name='twitter:image:alt' content={description} />
 				</Head>
