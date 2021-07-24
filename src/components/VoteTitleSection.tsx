d80b806c1723b19b731fc9596701673209348a22
Caleb Lovell <caleblovell1@gmail.com>
2021-07-24 15:49:40 -0600

    fix bug

--- src/components/VoteTitleSection.tsx
+++ src/components/VoteTitleSection.tsx
@@ -8,14 +8,38 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
 const localeObject = {
+	name: `es`, // name String
+	weekdays: `Domingo_Lunes ...`.split(`_`), // weekdays Array
+	weekdaysShort: `Sun_M`.split(`_`), // OPTIONAL, short weekdays Array, use first three letters if not provided
+	weekdaysMin: `Su_Mo`.split(`_`), // OPTIONAL, min weekdays Array, use first two letters if not provided
+	weekStart: 1, // OPTIONAL, set the start of a week. If the value is 1, Monday will be the start of week instead of Sunday。
+	yearStart: 4, // OPTIONAL, the week that contains Jan 4th is the first week of the year.
+	months: `Enero_Febrero ... `.split(`_`), // months Array
+	monthsShort: `Jan_F`.split(`_`), // OPTIONAL, short months Array, use first three letters if not provided
+	ordinal: n => `${n}º`, // ordinal Function (number) => return number + output
+	formats: {
+		// abbreviated format options allowing localization
+		LTS: `h:mm:ss A`,
+		LT: `h:mm A`,
+		L: `MM/DD/YYYY`,
+		LL: `MMMM D, YYYY`,
+		LLL: `MMMM D, YYYY h:mm A`,
+		LLLL: `dddd, MMMM D, YYYY h:mm A`,
+		// lowercase/short, optional formats for localization
+		l: `D/M/YYYY`,
+		ll: `D MMM, YYYY`,
+		lll: `D MMM, YYYY h:mm A`,
+		llll: `ddd, MMM D, YYYY h:mm A`,
+	},
 	relativeTime: {
-		future: `in %s`,
+		// relative time format strings, keep %s %d as the same
+		future: `in %s`, // e.g. in 2 hours, %s been replaced with 2hours
 		past: `%s ago`,
 		s: `a few seconds`,
 		m: `a minute`,
 		mm: `%d minutes`,
 		h: `an hour`,
-		hh: `%d hours`,
+		hh: `%d hours`, // e.g. 2 hours, %d been replaced with 2
 		d: `a day`,
 		dd: `%d days`,
 		M: `a month`,
@@ -23,9 +47,13 @@ const localeObject = {
 		y: `a year`,
 		yy: `%d years`,
 	},
+	meridiem: (hour) => {
+		// OPTIONAL, AM/PM
+		return hour > 12 ? `PM` : `AM`;
+	},
 };
 
-dayjs.locale(`en`, localeObject);
+dayjs.locale(localeObject, undefined, true);
 
 // import { useTranslation } from 'react-i18next';
 dayjs.extend(relativeTime);
